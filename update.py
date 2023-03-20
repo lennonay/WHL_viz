137ef3fc3aaa18e6f6d00d89595da170bb1e27fc
lennonay <lennon.auyeung@me.com>
2023-03-20 14:58:33 -0700

    add updater

---
+++ update.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+def updater():
+    today = datetime.today().strftime('%Y-%m-%d')
+    file1 = open("data/update.txt", "a")
+    file1.write('last updated: ' + today + '\n')
+    file1.close()
+
+if __name__ == '__main__':
+    updater()
