fa5f5df859b6b8dc50651a500ccd21c0115b22ac
lennonay <lennon.auyeung@me.com>
2023-03-21 21:27:32 -0700

    update to write

--- update.py
+++ update.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 def updater():
     today = datetime.today().strftime('%Y-%m-%d')
-    file1 = open("data/update.txt", "a")
+    file1 = open("data/update.txt", "w")
     file1.write('last updated: ' + today + '\n')
     file1.close()
 
